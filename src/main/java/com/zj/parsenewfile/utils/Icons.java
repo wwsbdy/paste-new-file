f6e4daa6e89660e0f3e557018210b8299500de86
jie.zhou <jie.zhou@alwaysmkt.com>
2025-09-25 10:19:18 +0800

    logo

---
+++ src/main/java/com/zj/parsenewfile/utils/Icons.java
@@ -0,0 +1,14 @@
+package com.zj.parsenewfile.utils;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author : jie.zhou
+ * @date : 2025/9/24
+ */
+public class Icons {
+
+    public static final Icon LOGO = IconLoader.getIcon("/icons/logo.svg", Icons.class);
+}
